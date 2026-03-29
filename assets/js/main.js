aed7abd5586c54bb5722d064e5224638031f4845
bakuyam <miebakuya1@gmail.com>
2026-03-29 20:19:11 +0000

    6.3 module

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,21 @@
 // main.js
 
-let rotation= 0;
-document.addEventListener("click", function () {
-    let min= 10;
-    let max= 40;
-    rotation += randomNumber(1,200);
-    document.body.style.transform= 'rotate(${rotation}deg)';
-});
-
 function randomNumber (min,max){
     return Math.floor(Math.random() * (max-min)+1) + min;
+}
+let divs= document.querySelectorAll("grid-container div");
+function randomize(){
+    divs.forEach(function(div){
+        let scale= randomNumber(.5,1.2);
+        lrt translateX= randomNumber(0,50);
+        let translateY= randomNumber(0,20);
+        letrotate= randomNumber(0,360);
+        div.style.transform=
+            'scale(${scale})
+            translateY(${translateX}%, ${translateY}%)
+            SpeechRecognitionAlternative(${rotate}deg)';
+    })
+    //randomize();
+document.addEventListener("click",randomize;)
+    console.log(divs.length, "randomize!")
 }
